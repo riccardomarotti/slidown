d06947f4c5987e966ea2e8a4a6b0feccad4be74f
Riccardo <riccardo.marotti@gmail.com>
2016-02-29 21:43:49 +0100

    Remove UnitTest classes in tests

--- slidown/tests/test_qpixmap.py
+++ slidown/tests/test_qpixmap.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+from nose.tools import assert_equals
 import unittest.mock
 
 from PyQt5 import QtWidgets
@@ -10,29 +10,27 @@ app = QtWidgets.QApplication([])
 from .. import qimage
 
 
-class QImageTest(unittest.TestCase):
+def test_image_from_html():
+    mock_webkit = unittest.mock.Mock(name='webkit')
+    mock_painter = unittest.mock.Mock(name='painter')
+    mock_page = unittest.mock.Mock(name='page')
+    mock_frame = unittest.mock.Mock(name='frame')
 
-    def test_image_from_html(self):
-        mock_webkit = unittest.mock.Mock(name='webkit')
-        mock_painter = unittest.mock.Mock(name='painter')
-        mock_page = unittest.mock.Mock(name='page')
-        mock_frame = unittest.mock.Mock(name='frame')
+    mock_webkit.page.return_value = mock_page
+    mock_page.mainFrame.return_value = mock_frame
+    mock_frame.contentsSize.return_value = 'a content size'
 
-        mock_webkit.page.return_value = mock_page
-        mock_page.mainFrame.return_value = mock_frame
-        mock_frame.contentsSize.return_value = 'a content size'
+    with unittest.mock.patch('PyQt5.QtGui.QImage') as mock_qimage:
+        actual_image_result = qimage.from_html('<h1> A Title </h1>',
+                                               mock_webkit,
+                                               mock_painter)
 
-        with unittest.mock.patch('PyQt5.QtGui.QImage') as mock_qimage:
-            actual_image_result = qimage.from_html('<h1> A Title </h1>',
-                                                   mock_webkit,
-                                                   mock_painter)
+        mock_webkit.setHtml.assert_called_with('<h1> A Title </h1>')
+        mock_painter.begin.assert_called_with(mock_qimage.return_value)
+        mock_frame.render.assert_called_with(mock_painter)
+        mock_painter.end.assert_called_with()
 
-            mock_webkit.setHtml.assert_called_with('<h1> A Title </h1>')
-            mock_painter.begin.assert_called_with(mock_qimage.return_value)
-            mock_frame.render.assert_called_with(mock_painter)
-            mock_painter.end.assert_called_with()
-
-            self.assertEqual(mock_qimage.return_value, actual_image_result)
+        assert_equals(mock_qimage.return_value, actual_image_result)
 
 
 
--- slidown/tests/test_slides.py
+++ slidown/tests/test_slides.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+from nose.tools import assert_equals
 from .. import slides
 
-class Slides(unittest.TestCase):
-    def test_simple_convert(self):
-        markdown = "#Title"
-        expected_output = '<h1>Title</h1>'
+def test_simple_convert():
+  markdown = "#Title"
+  expected_output = '<h1>Title</h1>'
 
-        self.assertEqual(expected_output, slides.html_from_markdown(markdown))
+  assert_equals(expected_output, slides.html_from_markdown(markdown))
 
-    def test_split(self):
-        markdown = '\
+def test_split():
+  markdown = '\
 # Title 1\n---\n\
 # Title 2\n-------- \n\
 # Title 3\n***\n\
@@ -20,16 +19,16 @@ class Slides(unittest.TestCase):
 # Title 6\n*****\n\
 # Title 7'
 
-        expected_output = ['<h1>Title 1</h1>',
-                           '<h1>Title 2</h1>',
-                           '<h1>Title 3</h1>',
-                           '<h1>Title 4</h1>',
-                           '<h1>Title 5</h1>',
-                           '<h1>Title 6</h1>',
-                           '<h1>Title 7</h1>']
+  expected_output = ['<h1>Title 1</h1>',
+                     '<h1>Title 2</h1>',
+                     '<h1>Title 3</h1>',
+                     '<h1>Title 4</h1>',
+                     '<h1>Title 5</h1>',
+                     '<h1>Title 6</h1>',
+                     '<h1>Title 7</h1>']
 
-        self.assertEqual(expected_output,
-                         slides.slides_from_html(slides.html_from_markdown(markdown)))
+  assert_equals(expected_output,
+                slides.slides_from_html(slides.html_from_markdown(markdown)))
 
 
 if __name__ == "__main__":
