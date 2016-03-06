d126bdf687d5f64c51a5d6ffd394e1175e64ce15
Riccardo <riccardo.marotti@gmail.com>
2016-03-06 17:05:40 +0100

    Add a couple of tests

--- slidown/tests/test_core.py
+++ slidown/tests/test_core.py
@@ -5,8 +5,39 @@ from nose.tools import assert_equals
 
 from .. import core
 
+def test_get_changed_slide_with_same_html():
+    html1 = '\
+<section class="slide">\
+<h1> A title </h1>\
+</section>\
+'
+    html2 = '\
+<section class="slide">\
+<h1> A title </h1>\
+</section>\
+'
+    assert_equals(-1, core.get_changed_slide(html1, html2))
 
-def test_get_changed_slide():
+def test_get_changed_slide_simple():
+    html0 = '\
+<section class="slide">\
+<h1> A title </h1>\
+</section>\
+<section class="slide">\
+<h1> Second title </h1>\
+</section>\
+'
+    html1 = '\
+<section class="slide">\
+<h1> A title </h1>\
+</section>\
+<section class="slide">\
+<h1> A different title title </h1>\
+</section>\
+'
+    assert_equals(1, core.get_changed_slide(html0, html1))
+
+def test_get_changed_slide_with_file():
     current_dir = os.path.dirname(__file__)
     html1 = open(os.path.join(current_dir, 'test_file1.html')).read()
     html2 = open(os.path.join(current_dir, 'test_file2.html')).read()
