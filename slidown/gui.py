82afbed43cb924503745be45810a9edfdd799120
Riccardo <riccardo.marotti@gmail.com>
2016-03-07 20:21:45 +0100

    Refactor

--- slidown/gui.py
+++ slidown/gui.py
@@ -9,15 +9,28 @@ def full_screen_change(mode, main_widget):
     {QtCore.Qt.Checked: main_widget.showFullScreen,
      QtCore.Qt.Unchecked: main_widget.showNormal}[mode]()
 
-def mode_change(mode, widget):
-    opacity = {QtCore.Qt.Checked: 0.8,
-               QtCore.Qt.Unchecked: 1} [mode]
-    widget.setWindowOpacity(opacity)
+def mode_change(edit_checkbox_state, widget):
+    function = {QtCore.Qt.Checked: set_edit_mode,
+                QtCore.Qt.Unchecked: unset_edit_mode} [edit_checkbox_state]
+    function(widget)
+
+def unset_edit_mode(widget):
+    widget.setWindowOpacity(1)
 
     geometry = widget.geometry()
 
-    window_flags = {QtCore.Qt.Checked: widget.windowFlags() | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint,
-                    QtCore.Qt.Unchecked: QtCore.Qt.Window & ~(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint) } [mode]
+    window_flags = QtCore.Qt.Window & ~(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
+    widget.setWindowFlags(window_flags)
+
+    widget.setGeometry(geometry)
+    widget.show()
+
+def set_edit_mode(widget):
+    widget.setWindowOpacity(0.8)
+
+    geometry = widget.geometry()
+
+    window_flags = widget.windowFlags() | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint
     widget.setWindowFlags(window_flags)
 
     widget.setGeometry(geometry)
