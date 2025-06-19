a067eb010c8d0d98d2f5e38a27b62c21800aea77
Riccardo <riccardo.marotti@gmail.com>
2025-06-19 16:22:07 +0200

    Remove system libraries from PyInstaller bundle to fix compatibility issues

--- delete_unused_libraries.py
+++ delete_unused_libraries.py
@@ -8,12 +8,35 @@ if len(argv) != 2:
     print(argv[0] + ": directory")
     exit(0)
 
+# Find unused libraries
 files_to_delete = set(os.popen('find '+ argv[1]  + ' -maxdepth 1 -not -type d | xargs ldd -u | cut -d "/" -s -f 4 ').readlines())
 files_to_delete = map(lambda file: os.path.join(argv[1], file).strip(),
                       files_to_delete)
 
+# Also remove problematic system libraries that can conflict
+system_libs_to_remove = [
+    'libstdc++.so.6',
+    'libgcc_s.so.1', 
+    'libc.so.6',
+    'libm.so.6',
+    'libpthread.so.0',
+    'libdl.so.2',
+    'librt.so.1'
+]
+
 for file in files_to_delete:
     try:
         os.remove(file)
+        print(f"Removed unused: {file}")
     except:
         pass
+
+# Remove system libraries that can cause version conflicts
+for lib in system_libs_to_remove:
+    lib_path = os.path.join(argv[1], lib)
+    if os.path.exists(lib_path):
+        try:
+            os.remove(lib_path)
+            print(f"Removed system lib: {lib_path}")
+        except:
+            pass
