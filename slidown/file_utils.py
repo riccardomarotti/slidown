7c6ede6dcaa65c6fca9801729aeefc6321d5bd71
Riccardo <riccardo.marotti@gmail.com>
2025-06-26 12:25:32 +0200

    Fix: Use QDesktopServices to open files in packaged app

--- slidown/file_utils.py
+++ slidown/file_utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os, sys, subprocess
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
 
 def add_md_extension(file_name):
     return '.'.join([file_name.split('.')[0], 'md'])
@@ -10,10 +12,7 @@ def touch(file_name):
         os.utime(file_name, None)
 
 def start(file_name):
-    open_functions = {
-        'darwin': lambda file_name: subprocess.run(['open', file_name]),
-        'linux': lambda file_name: subprocess.run(['xdg-open', file_name]),
-        'win32': lambda file_name: os.startfile(file_name)
-    }
+    # Use QDesktopServices for opening files/URLs, as it's more robust in Qt applications
+    # and handles cross-platform differences internally.
+    QDesktopServices.openUrl(QUrl.fromLocalFile(file_name))
 
-    open_functions[sys.platform](file_name)
